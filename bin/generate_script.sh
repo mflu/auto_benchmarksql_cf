618c9d0e57f6d77969e4887234d30ffb07ada8ed
Frank Lu <flu@vmware.com>
2012-12-17 03:24:23 +0000

    refactor it!

--- bin/generate_script.sh
+++ bin/generate_script.sh
@@ -3,25 +3,28 @@ bin_dir=`dirname $0`
 source $bin_dir/../config/common
 
 idx=0
-for i in `seq 1 $inst_heavy`
+for inst_type in heavy meduim light idle
 do
-  script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.heavy.$i
-  $base_dir/deploy/script_generator.sh $idx $script_file "heavy"
-  let idx++
+  echo "Generate script file for instances with type: ${inst_type}"
+  inst_type_var_name="inst_${inst_type}"
+  for i in `seq 1 ${!inst_type_var_name}`
+  do
+    script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.${inst_type}.$i
+    $base_dir/deploy/script_generator.sh $idx $script_file "${inst_type}"
+    let idx++
+  done
 done
 
-for i in `seq 1 $inst_medium`
-do
-  script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.medium.$i
-  $base_dir/deploy/script_generator.sh $idx $script_file  "medium"
-  let idx++
-done
+#for i in `seq 1 $inst_medium`
+#do
+#  script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.medium.$i
+#  $base_dir/deploy/script_generator.sh $idx $script_file  "medium"
+#  let idx++
+#done
 
-for i in `seq 1 $inst_light`
-do
-  script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.light.$i
-  $base_dir/deploy/script_generator.sh $idx $script_file  "light"
-  let idx++
-done
-
-
+#for i in `seq 1 $inst_light`
+#do
+#  script_file=$base_dir/var/$local_script_dir/${service_type}.${inst_num}.light.$i
+#  $base_dir/deploy/script_generator.sh $idx $script_file  "light"
+#  let idx++
+#done
